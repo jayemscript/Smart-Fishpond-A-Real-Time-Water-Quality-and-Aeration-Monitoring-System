ef3ce48da90cabed485df2b54904c4dd2a9b84c1
GitHubNexon <johnmarkpulmano052802@gmail.com>
2026-03-08 19:25:12 +0800

    fix sensor service

--- server/src/modules/sensors/services/sensor-alert.service.ts
+++ server/src/modules/sensors/services/sensor-alert.service.ts
@@ -39,26 +39,51 @@ export const ALERT_CONFIG: Record<
     minAbnormal: number; // min bad readings to trigger alert
     cooldownMs: number; // silence period after alert fires
   }
+  // testing
+  // > = {
+  //   temperature: {
+  //     windowMs: 1 * 60 * 1000, // 10 minutes
+  //     minAbnormal: 15, // 15 bad readings in window
+  //     cooldownMs: 1 * 60 * 1000, // 30 min cooldown
+  //   },
+  //   phWater: {
+  //     windowMs: 1 * 60 * 1000,
+  //     minAbnormal: 15,
+  //     cooldownMs: 1 * 60 * 1000,
+  //   },
+  //   dissolvedOxygen: {
+  //     windowMs: 1 * 60 * 1000,
+  //     minAbnormal: 10,
+  //     cooldownMs: 1 * 60 * 1000,
+  //   },
+  //   waterLevel: {
+  //     windowMs: 1 * 60 * 1000, // 5 minutes
+  //     minAbnormal: 5, // 5 low readings in window
+  //     cooldownMs: 1 * 60 * 1000,
+  //   },
+  // };
+
+  //original
 > = {
   temperature: {
-    windowMs: 1 * 60 * 1000, // 10 minutes
+    windowMs: 10 * 60 * 1000, // 10 minutes
     minAbnormal: 15, // 15 bad readings in window
-    cooldownMs: 1 * 60 * 1000, // 30 min cooldown
+    cooldownMs: 30 * 60 * 1000, // 30 min cooldown
   },
   phWater: {
-    windowMs: 1 * 60 * 1000,
+    windowMs: 10 * 60 * 1000,
     minAbnormal: 15,
     cooldownMs: 1 * 60 * 1000,
   },
   dissolvedOxygen: {
-    windowMs: 1 * 60 * 1000,
+    windowMs: 10 * 60 * 1000,
     minAbnormal: 10,
     cooldownMs: 1 * 60 * 1000,
   },
   waterLevel: {
-    windowMs: 1 * 60 * 1000, // 5 minutes
+    windowMs: 10 * 60 * 1000, // 5 minutes
     minAbnormal: 5, // 5 low readings in window
-    cooldownMs: 1 * 60 * 1000,
+    cooldownMs: 30 * 60 * 1000,
   },
 };
 
